f7a660cd0db4dc820b106618186e6209b13d8590
eungyukm <86348187+eungyukm@users.noreply.github.com>
2022-02-08 09:14:14 +0900

    Input System Update

---
+++ InputSystem/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var gamePad = Gamepad.current;
+        Vector2 move = gamePad.leftStick.ReadValue();
+
+        Debug.Log("move : " + move);
+    }
+}
